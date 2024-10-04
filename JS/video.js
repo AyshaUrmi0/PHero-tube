4a12872b541a2adb430196fe4e3fc8ef051a3922
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-10-04 12:35:13 +0900

    tumbnail and verified added

--- JS/video.js
+++ JS/video.js
@@ -10,11 +10,50 @@ const loadCategories=()=>{
 
 
 };
+const loadVideos=()=>{
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res)=>res.json())
+    .then((data)=>displayVideos(data.videos))
+   .catch((error)=>console.log(error))
 
-// {
-//     "category_id": "1001",
-//     "category": "Music"
-// }
+
+};
+
+
+// create displayVideos
+const displayVideos=(videos)=>{
+    const videoContainer=document.getElementById("videos");
+    console.log(videoContainer);
+    
+    videos.forEach((video)=> {
+        const videoDiv=document.createElement("div");
+        videoDiv.classList="card card-compact";
+        videoDiv.innerHTML=
+        `  <figure class="h-[200px]">
+    <img class="h-full w-full object-cover"
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+
+    <div>
+      <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} alt=""/>
+
+    </div>
+    <div>
+      <h2 class="text-lg font-bold">${video.title}</h2>
+      <div class="flex items-center gap-2">
+      <p class="text-sm opacity-70">${video.authors[0].profile_name}</p>
+      ${video.authors[0].verified? `<img class="w-5" src=https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png alt=""/>`: ""}
+      </div>
+    </div>
+  </div>`;
+        videoContainer.appendChild(videoDiv);
+    });
+
+}
+
+
 
 // create DisplayCategories
 const displayCategories=(categories)=>{
@@ -34,4 +73,7 @@ const displayCategories=(categories)=>{
 
 loadCategories();
 
-displayCategories();
+
+
+loadVideos();
+
